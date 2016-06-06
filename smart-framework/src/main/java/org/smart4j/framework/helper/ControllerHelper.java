d2c3699fee7dd4936a8aa818cfc7cc072c60fcd0
shizhouyong <zhouyong.shi@gmail.com>
2016-06-06 22:18:49 +0800

    commit on 6.06 night

---
+++ smart-framework/src/main/java/org/smart4j/framework/helper/ControllerHelper.java
@@ -0,0 +1,11 @@
+package org.smart4j.framework.helper;
+
+/**
+ * Created by szy on 2016/6/6.
+ */
+public final class ControllerHelper {
+
+    /**
+     * 用于存放请求与处理器的映射关系（简称Action Map）
+     */
+}
